b1ad32869acfb59d9d559fc12288bc16c3ef64c8
szrharrison <s.z.r.harrison@gmail.com>
2017-05-09 15:15:42 -0400

    Fixed custom highlighting function

--- hunt-and-peck-app/app.js
+++ hunt-and-peck-app/app.js
@@ -3,7 +3,6 @@ $(function() {
 
   $('#input').on('keydown.firstKey', function(e){
     if(e.keyCode == 32){
-      console.log('SB')
     } else {
       setTimer()
       $('#input').off('keydown.firstKey')
@@ -14,7 +13,6 @@ $(function() {
 
 function trackKeys() {
   $('#input').on('keydown.trackKeys', function(e) {
-    console.log(e.keyCode)
     if(e.keyCode === 32) {
       $(this).val('')
     }
@@ -66,6 +64,7 @@ jQuery.fn.highlight = function(pat) {
   }
   return skip;
  }
+ console.log( this )
  return this.length && pat && pat.length ? this.each(function() {
   innerHighlight(this, pat.toUpperCase());
  }) : this;
@@ -80,47 +79,47 @@ jQuery.fn.removeHighlight = function() {
   }
  }).end();
 };
-// function highlight($node, pattern) {
-//   let node = $node[0]
-//   console.log(node.nodeType)
-//  function innerHighlight(node, pattern) {
-//   var skip = 0
-//   if (node.nodeType == 3) {
-//    var pos = node.data.indexOf(pattern)
-//    if (pos >= 0) {
-//     var spannode = document.createElement('span')
-//     spannode.className = 'highlight'
-//     var middlebit = node.splitText(pos)
-//     var endbit = middlebit.splitText(pattern.length)
-//     var middleclone = middlebit.cloneNode(true)
-//     spannode.appendChild(middleclone)
-//     middlebit.parentNode.replaceChild(spannode, middlebit)
-//     skip = 1
-//    }
-//  } else {
-//   if (node.nodeType == 1 && node.childNodes && !/(script|style)/i.test(node.tagName)) {
-//     console.log('got here')
-//    for (var i = 0; i < node.childNodes.length; ++i) {
-//     i += innerHighlight(node.childNodes[i], pattern)
-//    }
-//   }
-//   return skip
-//  }
-//  if (node.length && pattern && pattern.length) {
-//    return node.each(function() {
-//     innerHighlight(node, pattern.toUpperCase());
-//   })
-//   } else {
-//     return node
-//   }
-// };
-//
-// function removeHighlight(node) {
-//  return node.find("span.highlight").each(function() {
-//   this.parentNode.firstChild.nodeName
-//   with (this.parentNode) {
-//    replaceChild(this.firstChild, this)
-//    normalize()
-//   }
-//  }).end()
-// };
+
+
+function highlight($nodes, pattern) {
+  function innerHighlight($node, pattern) {
+    var skip = 0
+    if ($node.nodeType == 3) {
+      var pos = $node.data.indexOf(pattern)
+      if (pos >= 0) {
+        var spannode = document.createElement('span')
+        spannode.className = 'highlight'
+        var middlebit = $node.splitText(pos)
+        var endbit = middlebit.splitText(pattern.length)
+        var middleclone = middlebit.cloneNode(true)
+        spannode.appendChild(middleclone)
+        middlebit.parentNode.replaceChild(spannode, middlebit)
+        skip = 1
+        return
+      }
+    } else if ($node.nodeType == 1 && $node.className !== 'highlight' && $node.childNodes && !/(script|style)/i.test($node.tagName)) {
+      for (var i = 0; i < $node.childNodes.length; ++i) {
+        i += innerHighlight($node.childNodes[i], pattern)
+      }
+    }
+    return skip
+  }
+
+  if ($nodes.length && pattern && pattern.length) {
+    return $nodes.each(function() {
+      innerHighlight(this, pattern)
+    })
+  } else {
+    return $nodes
+  }
+}
+
+function removeHighlight(node) {
+ return node.find("span.highlight").each(function() {
+  this.parentNode.firstChild.nodeName
+  with (this.parentNode) {
+   replaceChild(this.firstChild, this)
+   normalize()
+  }
+ }).end()
+}
