83cdde9586e902758a0908ce416e899324c75a49
szrharrison <s.z.r.harrison@gmail.com>
2017-05-09 12:07:35 -0400

    Made some CSS changes

--- hunt-and-peck-app/app.js
+++ hunt-and-peck-app/app.js
@@ -45,42 +45,41 @@ function setTimer() {
 
 jQuery.fn.highlight = function(pat) {
  function innerHighlight(node, pat) {
-  var skip = 0
+  var skip = 0;
   if (node.nodeType == 3) {
-   var pos = node.data.toUpperCase().indexOf(pat)
+   var pos = node.data.toUpperCase().indexOf(pat);
    if (pos >= 0) {
-    var spannode = document.createElement('span')
-    spannode.className = 'highlight'
-    var middlebit = node.splitText(pos)
-    var endbit = middlebit.splitText(pat.length)
-    var middleclone = middlebit.cloneNode(true)
-    spannode.appendChild(middleclone)
-    middlebit.parentNode.replaceChild(spannode, middlebit)
-    skip = 1
+    var spannode = document.createElement('span');
+    spannode.className = 'highlight';
+    var middlebit = node.splitText(pos);
+    var endbit = middlebit.splitText(pat.length);
+    var middleclone = middlebit.cloneNode(true);
+    spannode.appendChild(middleclone);
+    middlebit.parentNode.replaceChild(spannode, middlebit);
+    skip = 1;
    }
   }
   else if (node.nodeType == 1 && node.childNodes && !/(script|style)/i.test(node.tagName)) {
    for (var i = 0; i < node.childNodes.length; ++i) {
-    i += innerHighlight(node.childNodes[i], pat)
+    i += innerHighlight(node.childNodes[i], pat);
    }
   }
-  return skip
+  return skip;
  }
  return this.length && pat && pat.length ? this.each(function() {
-  innerHighlight(this, pat.toUpperCase())
- }) : this
+  innerHighlight(this, pat.toUpperCase());
+ }) : this;
 };
 
 jQuery.fn.removeHighlight = function() {
  return this.find("span.highlight").each(function() {
-  this.parentNode.firstChild.nodeName
+  this.parentNode.firstChild.nodeName;
   with (this.parentNode) {
-   replaceChild(this.firstChild, this)
-   normalize()
+   replaceChild(this.firstChild, this);
+   normalize();
   }
- }).end()
-}
-
+ }).end();
+};
 // function highlight($node, pattern) {
 //   let node = $node[0]
 //   console.log(node.nodeType)
