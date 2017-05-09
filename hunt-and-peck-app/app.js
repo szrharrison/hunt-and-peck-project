a104b26375c425daf8dfb0e127660ebcab7e1395
Naz Khan <nazmuskhan@yahoo.com>
2017-05-09 15:16:08 -0400

    more work

--- hunt-and-peck-app/app.js
+++ hunt-and-peck-app/app.js
@@ -1,24 +1,54 @@
 $(function() {
   getParagraph()
 
+
   $('#input').on('keydown.firstKey', function(e){
     if(e.keyCode == 32){
       console.log('SB')
     } else {
+
       setTimer()
-      $('#input').off('keydown.firstKey')
       trackKeys()
+      $('#input').off('keydown.firstKey')
     }
   })
 })
 
 function trackKeys() {
+  const text = $('#test').text().split(" ")
+  let counter = 0
+  let matchArray = []
   $('#input').on('keydown.trackKeys', function(e) {
     console.log(e.keyCode)
     if(e.keyCode === 32) {
+      let userInput = $(this).val()
+      if (userInput === text[counter]){
+        matchArray.push(1)
+        console.log("PUSHED 1")
+      }else{
+        matchArray.push(0)
+        console.log("PUSHED 2")
+      }
+      counter ++
+      // console.log(userInput)
+      // textMatch(userInput)
       $(this).val('')
     }
   })
+}
+
+
+function textMatch(userInput){
+  let match = []
+  //iterate through paragraph array to see if words match?
+  //something like this?
+  for (let i=0; i< text.length; i++){
+    if (userInput === text[i]){
+      match.push(true)
+    } else
+      match.push(false)
+  }
+  // if match, make new array with true or falses as elements?
 }
 
 function getParagraph() {
@@ -45,41 +75,42 @@ function setTimer() {
 
 jQuery.fn.highlight = function(pat) {
  function innerHighlight(node, pat) {
-  var skip = 0;
+  var skip = 0
   if (node.nodeType == 3) {
-   var pos = node.data.toUpperCase().indexOf(pat);
+   var pos = node.data.toUpperCase().indexOf(pat)
    if (pos >= 0) {
-    var spannode = document.createElement('span');
-    spannode.className = 'highlight';
-    var middlebit = node.splitText(pos);
-    var endbit = middlebit.splitText(pat.length);
-    var middleclone = middlebit.cloneNode(true);
-    spannode.appendChild(middleclone);
-    middlebit.parentNode.replaceChild(spannode, middlebit);
-    skip = 1;
+    var spannode = document.createElement('span')
+    spannode.className = 'highlight'
+    var middlebit = node.splitText(pos)
+    var endbit = middlebit.splitText(pat.length)
+    var middleclone = middlebit.cloneNode(true)
+    spannode.appendChild(middleclone)
+    middlebit.parentNode.replaceChild(spannode, middlebit)
+    skip = 1
    }
   }
   else if (node.nodeType == 1 && node.childNodes && !/(script|style)/i.test(node.tagName)) {
    for (var i = 0; i < node.childNodes.length; ++i) {
-    i += innerHighlight(node.childNodes[i], pat);
+    i += innerHighlight(node.childNodes[i], pat)
    }
   }
-  return skip;
+  return skip
  }
  return this.length && pat && pat.length ? this.each(function() {
-  innerHighlight(this, pat.toUpperCase());
- }) : this;
+  innerHighlight(this, pat.toUpperCase())
+ }) : this
 };
 
 jQuery.fn.removeHighlight = function() {
  return this.find("span.highlight").each(function() {
-  this.parentNode.firstChild.nodeName;
+  this.parentNode.firstChild.nodeName
   with (this.parentNode) {
-   replaceChild(this.firstChild, this);
-   normalize();
+   replaceChild(this.firstChild, this)
+   normalize()
   }
- }).end();
-};
+ }).end()
+}
+
 // function highlight($node, pattern) {
 //   let node = $node[0]
 //   console.log(node.nodeType)
