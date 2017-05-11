29703da661ecef122dc76966d23569243ddc1b95
peteryunkim <github email address>
2017-05-11 10:17:27 -0400

    loaded seeds

--- hunt-and-peck-api/db/seeds.rb
+++ hunt-and-peck-api/db/seeds.rb
@@ -1,7 +1,96 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+p1 = "Pilots and passengers must wear heavy arctic clothing as few aircraft have cabin heat systems capable of coping
+with the cold. Aircraft batteries are removed at the end of each day of flying to ensure maximum efficiency the next
+day. Wings and engines are covered to stop the ingress of snow and the build up of frost on the flight surfaces.
+Gasoline fueled blow-pots are often carried to provide engine preheat to ensure starting. The oil in these engines gets
+so thick at minus to degrees that the engine cannot be turned. This writer has done chin-ups on the propeller of a
+coldsoaked Cessna which flew the next day after preheating.The northern airports lack any degree of services in
+comparison to those farther south. Fuel is usually available if you can locate the operator. Some of the larger
+communities may have a pay telephone at the airstrip, but the normal procedure is to buzz the town on arrival. This lets
+the people know you are landing and usually someone will head out to the strip to meet you.Unless you are a regular
+customer, all transactions for fuel and oil are on a cash basis. Cheques are nearly useless in a village without a bank.
+Credit to a stranger is foolhardy. During the grip of winter, the snowmobile is the major mode of transport in all but
+the largest of settlements. In many places that do have roads in winter, vehicles are left running hours a day. If
+allowed to get cold, it may take many hours of effort to restart balky engines."
+
+p2 = "they'd managed about a mile on the highway, so instead of a mile closer, he was a mile further away. That was two
+miles more than he had thought when he jumped from the truck. Make it four miles, then add another for bad luck and he
+was easily a fool for going on. He ought to turn back, get in the truck and explain to Diana his idiocy. They could hold
+each other and talk and joke the afternoon and night time away, lighting one candle from the stub of the last, keeping
+each other warm under the scratchy wool of the army surplus blankets as they waited for the daylight and the end of the
+storm; the inevitable snowplow followed by a Wyoming Highway Patrol car equipped with food and drink. Diana lit the
+candle just like Dan told her to. She watched as he turned first one way, then the other. He hesitated, walked to the
+left, hesitated again and she hoped he would turn around and come back to her but she knew he wouldn't. He had decided
+to go to Casper for help and Dan always did what he set out to do. She held the candle in her hand as she watched the
+back of his parka until it vanished with startling suddenness in the snow. She held the candle in her hand so that its
+light shined through the window for a long time after she could see nothing but the snow racing ahead of the wind
+because perhaps he could see it and know that she waited in trust for him to return."
+
+p3 = "The drift trailed from the big airport sign. Had he not stumbled, he'd have gone right past, leaving another mile
+before the next possible help.  His feet were no longer a part of him, the skin of his forehead rasped when he touched
+it with his mittens and his legs felt both rubbery and wooden simultaneously. The road ran north a half mile to the
+terminal with nothing but a few leafless and spindly trees planted to either side to break the wind.  It cut now from
+his left front quarter.  He tucked his chin deeply into his right shoulder, wrapped his arms over his face, and slogged
+on.  Perhaps a baby would complete the perfection of her. Their life had begun to seem too large, too loose. And they
+sometimes rattled against each other like the last two hard dried kernels in a feed bucket. Maybe he was halfway to the
+terminal. It might as well be halfway to the moon. He was exhausted, his breath dragged through the bandana in hard
+gasps. He couldn't pull it down to breathe more freely. The bandana was stuck to his frozen nose and cheeks. Diana loved
+kids, that was certain, and the kids at the School for the Deaf surely loved her.He grinned at that. Just last week,
+Diana had been all bubbly excitement over the planning for next month's annual end school tour and picnic. As
+teacher's aide, she would make sure that all the kids were dressed and equipped right for the ride to the Dave Johnson
+power plant and the picnic and fishing in the little park"
+
+p4 = "Buster Crab and Sam Clam were the best of friends for years, and every evening one could find Buster in Sam's bar
+down by the beach. Finally,Buster passed on and went to heaven.  There, he was a model citizen, but he longed for the
+companionship of his pal, Sam.  So the next Christmas, Buster was approached by God who asked him why he wasn't happy.
+Buster explained that although heaven was a pretty nice place and the halo and wings were real swell, he missed Sam and
+wished he could go back and spend just one more night in his bar by the beach.  God recalled how good Buster had been,
+and told him that he could spend New Year's with Sam in his bar. However, God cautioned, things have changed since
+you were there last. In order to keep up with the times, Sam has converted his place to a disco. Still, if you wish, you
+may spend New Year's eve with your old buddy, but be sure not to drink and take good care of your wings, harp and halo.
+Buster was ecstatic and spend the next week practicing his chops on the harp and polishing his halo. Then the big eveing
+arrived and poof! there he was beside Sam in his disco. Well, they had a great time talking of old timesand dancing and
+singing and playing. After a while, the temptation to have a drink proved too great for Buster, and he imbibed. So did
+the rest of the party and the night got progressively wilder until morning found everyone sleeping it off on the
+furniture and floors of the disco. His time up, Buster was transported poof! back to heaven. God saw him, bleary eyed,
+halo dented and slipping off to one side, wings in disarray, and well, you get the picture.  God gives him an ice bag
+and asks what happened. Buster! You've been at the bottle, haven't you?  Look at you wings! Look at that halo! And
+where is you golden harp? To which Buster replies, I left my harp in Sam Clam's Disco."
+
+p5 = "The air was filled with a combination of noxious odors that he  recognized but was unaccustomed to smelling on a
+regular basis.  The offensive stench of urine from the alleyways, the odious scent of rotting garbage and filth that
+overflowed the dumpsters and spilled into the street combined with the stale smell of alcohol and vomit to make his
+stomach lurch.  Perhaps he was not as prepared for this as he had thought, but he was committed to this endeavor and
+he'd follow through. He traveled down Franklin Street to the Hobo encampment that he'd been to earlier, beneath the
+Harding overpass and told several people that he was searching for Rose Bellman. The first two men he'd asked were from
+out of town, the next three people refused to even look at him and the woman he'd approached backed away from him in
+fear. Discouraged, he sat down and leaned his back up against one of the overpass supports and placed in head in his
+hands. Maybe Dana was right. maybe he couldn't be convincing enough to pull this off. Lying wasn't exactly his forte, in
+fact she had told him more than once that he'd be terrible as an undercover agent for just that reason. Subterfuge
+always went against his basic nature and the guilt he felt in its performance seemed to surface unconsciously in his
+body language and expression. He sighed in frustration and anger at life's seeming unfairness at wasted lives needless
+deaths his own inadequacies that he felt prevented him from solving this case. Lost in his thoughts, he failed to hear
+the approaching footsteps that halted before him until the voice brought him back to reality."
+
+p6 = "When I look at the three massive manuscript volumes which contain our work for the year 1894, I confess that it is
+very difficult for me, out of such a wealth of material, to select the cases which are most interesting in themselves,
+and at the same time most conducive to a display of those peculiar powers for which my friend was famous. As I turn over
+the pages, I see my notes upon the repulsive story of the red leech and the terrible death of Crosby, the banker. Here
+also I find an account of the Addleton tragedy, and the singular contents of the ancient British barrow. The famous
+succession case comes also within this period, and so does the tracking and arrest of the Boulevard assassin an exploit
+which won for Holmes an autograph letter of thanks from the French President and the Order of the Legion of Honour. Each
+of these would furnish a narrative, but on the whole I am of opinion that none of them unites so many singular points of
+interest as the episode of Yoxley Old Place, which includes not only the lamentable death of young Willoughby Smith, but
+also those subsequent developments which threw so curious a light upon the causes of the crime. It was a wild, night
+towards the close of November. Holmes and I sat together in silence all the evening, he engaged with a powerful lens
+deciphering the remains of the original inscription upon a palimpsest, I deep in a recent treatise upon surgery. Outside
+the wind howled down Baker Street, while the rain beat fiercely against the windows. It was strange there, in the very
+depths of the town, with ten miles of man's handiwork on every side of us, to feel the iron grip of Nature, and to be
+conscious that to the huge elemental forces all London was no more than the molehills that dot the fields."
+
+# Paragraph.create(content: p1)
+# Paragraph.create(content: p2)
+# Paragraph.create(content: p3)
+# Paragraph.create(content: p4)
+# Paragraph.create(content: p5)
+Paragraph.create(content: p6)
+
